341c8bca1cdf38925f40f811f6c1cf6f1010af03
odrum428 <oxkeita@gmail.com>
2019-06-01 23:28:57 +0900

    追加設定を追記

--- portfolio/webpack.config.dev.js
+++ portfolio/webpack.config.dev.js
@@ -41,7 +41,7 @@ module.exports = {
             template: "index.html"
         })
     ],
-    server: {
+    serve: {
         content: path.resolve(__dirname, 'dist'),
         port: 3000,
     }
